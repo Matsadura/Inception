dd7b44de3bcd38efc97d44767e350e33728162f7
Matsadura <zidanditoo1@gmail.com>
2026-07-31 03:24:34 +0100

    [ADD] Implement dynamic port configuration for FTP service with entrypoint script

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+sed -i "s/FTP_PORT_PLACEHOLDER/${FTP_PORT}/g" /etc/vsftpd.conf
+sed -i "s/PASV_MIN_PORT_PLACEHOLDER/${FTP_PASV_MIN_PORT}/g" /etc/vsftpd.conf
+sed -i "s/PASV_MAX_PORT_PLACEHOLDER/${FTP_PASV_MAX_PORT}/g" /etc/vsftpd.conf
+
 if ! id "$FTP_USER" &>/dev/null; then
     adduser --disabled-password --gecos "" --home /var/www/wordpress --shell /bin/bash $FTP_USER
     echo "$FTP_USER:$(cat $FTP_PASSWORD_FILE)" | chpasswd
